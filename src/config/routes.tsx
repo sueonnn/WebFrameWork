f2797f7396ae402e55d1272fb8f1970da7083284
Jeong-dawon <dy030819@naver.com>
2025-11-06 10:34:17 +0900

    feat: 메인 페이지 구현

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import MainPage from '../pages/MainPage';
 import GroupCreatePage from "../pages/GroupCreatePage";
 import LoginPage from "../pages/LoginPage";
 
@@ -8,8 +9,8 @@ export const router = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
-      { index: true, element: <GroupCreatePage /> }, // 임시 홈 = 그룹 생성
-      { path: "/groups/new", element: <GroupCreatePage /> }, // 정식 경로
+      { index: true, element: <MainPage /> }, 
+      { path: "/groups/new", element: <GroupCreatePage /> }, 
       // { path: '/groups/:groupId', element: <GroupDetailPage /> }, // 추후 연결
     ],
   },
